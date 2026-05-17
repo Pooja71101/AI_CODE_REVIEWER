675f5e47216ea916c9965ad400a17491131fa67a
Pooja71101 <pooja71101@gmail.com>
2026-05-17 17:27:20 +0530

    switch to PostgreSQL on Render

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,25 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./reviews.db"
+# DATABASE_URL = "sqlite:///./reviews.db"
+DATABASE_URL =os.getenv("DATABASE_URL", "sqlite:///./reviews.db")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    pool_size=1,
-    max_overflow=0,
-)
+# engine = create_engine(
+#     DATABASE_URL,
+#     connect_args={"check_same_thread": False},
+#     pool_size=1,
+#     max_overflow=0,
+# )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Base = declarative_base()
